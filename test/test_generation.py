dea880bfaef1ac0ba68791d8de1a4c839a27d6e9
Paige Vegna <pvegna@princeton.edu>
2024-05-01 03:34:31 -0400

    train script

--- test/test_generation.py
+++ test/test_generation.py
@@ -1,12 +1,17 @@
 from transformers import pipeline
 target = "ot demees meht fo yna ro toillE rM erofeb ,elat emas eht gnillet ecnatsid a ta draeh eb ot gninnigeb saw namhctaw eht dna \u201d,sdnuos revlis sti htiw nevele\u201c kcurts dah eceip-letnam eht no kcolc elttil tnagele ehT .meht htiw ruoh na diats eH"
 prompt = ".gnol ereht neeb dah eh taht leef "
-forwards = prompt + target
-forwards = forwards[::-1]
+fwd_target = prompt + target
+fwd_target = fwd_target[::-1]
 generator = pipeline("text-generation", model="/scratch/network/pvegna/models/gpt2-backwards/checkpoint-1040", tokenizer="/scratch/network/pvegna/models/gpt2-tokenizer")
 gen = generator(prompt)[0]['generated_text']
-
+fwd_gen = prompt + gen
+fwd_gen = fwd_gen[::-1]
 print("raw target: " + target)
+print('------------------------------------')
 print("raw generation: " + gen)
-print("forwards target: " + forwards)
-print("forwards generation: " + (prompt + gen)[::-1])
+print('------------------------------------')
+print("forwards target: " + fwd_target)
+print('------------------------------------')
+print("forwards generation: " + fwd_gen)
+print('------------------------------------')
