f4ea6d401434246a23f0bce52fc73be2ef97ef2f
Paige Vegna <pvegna@princeton.edu>
2024-05-01 08:22:17 -0400

    test gen

--- test/test_generation.py
+++ test/test_generation.py
@@ -1,16 +1,19 @@
 from transformers import pipeline
-target = "ot demees meht fo yna ro toillE rM erofeb ,elat emas eht gnillet ecnatsid a ta draeh eb ot gninnigeb saw namhctaw eht dna \u201d,sdnuos revlis sti htiw nevele\u201c kcurts dah eceip-letnam eht no kcolc elttil tnagele ehT .meht htiw ruoh na diats eH"
-prompt = ".gnol ereht neeb dah eh taht leef "
-fwd_target = prompt + target
-fwd_target = fwd_target[::-1]
+target = ".gnol ereht neeb dah eh taht leef ot demees meht fo yna ro toillE rM erofeb ,elat emas eht gnillet ecnatsid a ta draeh eb ot gninnigeb saw namhctaw eht dna \u201d,sdnuos revlis sti htiw nevele\u201c kcurts dah eceip-letnam eht no kcolc elttil tnagele ehT .meht htiw ruoh na diats eH"
+prompt = ""
+
 generator = pipeline("text-generation", model="/scratch/network/pvegna/models/gpt2-backwards/checkpoint-5200", tokenizer="/scratch/network/pvegna/models/gpt2-tokenizer")
-gen = generator(prompt)[0]['generated_text']
-fwd_gen = gen[::-1]
-print("raw target: " + target)
-print('------------------------------------')
-print("raw generation: " + gen)
-print('------------------------------------')
-print("forwards target: " + fwd_target)
-print('------------------------------------')
-print("forwards generation: " + fwd_gen)
-print('------------------------------------')
+
+with open('example_generations.txt', 'w') as out_file:
+    for i in range(10):
+        target = target.strip()
+        prompt += target[:target.index(' ')]
+        fwd_target = prompt + target
+        fwd_target = fwd_target[::-1]
+        gen = generator(prompt)[0]['generated_text']
+        fwd_gen = gen[::-1]
+        out_file.write('------------------------------------')
+        out_file.write("\nraw target: " + target)
+        out_file.write("\nraw generation: " + gen)
+        out_file.write("\nforwards target: " + fwd_target)
+        out_file.write("\nforwards generation: " + fwd_gen)
