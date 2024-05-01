fcffaabb4929ca26f9d366b6eb454bdc84aea91e
Paige Vegna <pvegna@princeton.edu>
2024-05-01 09:25:18 -0400

    fwd test

--- test/fwd_generation.py
+++ test/fwd_generation.py
@@ -2,9 +2,9 @@ from transformers import pipeline
 target = "Husbands and wives generally understand when opposition will be vain. Mary knew, from Charles\u2019s manner of speaking, that he was quite determined on going, and that it would be of no use to teaze him. She said nothing, therefore, till he was out of the room, but as soon as there was only Anne to hear\u2014"
 prompt = ""
 
-generator = pipeline("text-generation", model="/scratch/network/pvegna/models/gpt2-forwards/checkpoint-5200", tokenizer="/scratch/network/pvegna/models/gpt2-tokenizer")
+generator = pipeline("text-generation", model="/scratch/network/pvegna/models/gpt2", tokenizer="/scratch/network/pvegna/models/gpt2-tokenizer")
 
-with open('fwd_generations.txt', 'w') as out_file:
+with open('fwd_base_generations.txt', 'w') as out_file:
     for i in range(10):
         target = target.strip()
         prompt += ' ' + target[:target.index(' ')]
