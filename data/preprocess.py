4af16b5f17b7e9a34554742096237a37552954c8
Paige Vegna <pvegna@princeton.edu>
2024-05-01 03:12:43 -0400

    train script

--- data/preprocess.py
+++ data/preprocess.py
@@ -2,10 +2,11 @@ import json
 with open('persuasion.txt', 'r', encoding='utf-8') as in_file:
     data = in_file.read()
 
-with open('temp.json', 'w', encoding='utf-8') as out_file:
+with open('temp_fwd.json', 'w', encoding='utf-8') as out_file:
     data = data.split('\n\n')
     for paragraph in data:
         text = paragraph.replace('\n', ' ')
         if text.strip() != '':
-            ex = {'text': text[::-1]}
+            #ex = {'text': text[::-1]}
+            ex = {'text': text}
             out_file.write(json.dumps(ex) + '\n')
