5664e7442ddecef86b6686d1a08d691b3a6de56d
Paige Vegna <pvegna@princeton.edu>
2024-05-05 02:26:08 -0400

    more data

--- data/partition.py
+++ data/partition.py
@@ -1,10 +1,10 @@
 import random
-with open('temp.json', 'r') as in_file:
+with open('temp_bwd.json', 'r') as in_file:
     data = in_file.readlines()
 
 random.shuffle(data)
-train = data[:int(len(data) * 0.8)]
-test = data[int(len(data) * 0.8):]
+train = data[:int(len(data) * 0.85)]
+test = data[int(len(data) * 0.85):]
 with open('train.json', 'w') as out_file:
     for ex in train:
         out_file.write(ex)
--- data/preprocess.py
+++ data/preprocess.py
@@ -1,12 +1,22 @@
+import os
 import json
-with open('persuasion.txt', 'r', encoding='utf-8') as in_file:
-    data = in_file.read()
+ 
+# iterate over files in
+# that directory
+data = []
+directory = 'data'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    with open(f, 'r', encoding='utf-8') as in_file:
+        data.append(in_file.read())
 
-with open('temp_fwd.json', 'w', encoding='utf-8') as out_file:
-    data = data.split('\n\n')
-    for paragraph in data:
-        text = paragraph.replace('\n', ' ')
-        if text.strip() != '':
-            #ex = {'text': text[::-1]}
-            ex = {'text': text}
-            out_file.write(json.dumps(ex) + '\n')
+with open('temp_bwd.json', 'w', encoding='utf-8') as out_file:
+    for d in data:
+        d = d.split('\n\n')
+        for paragraph in d:
+            text = paragraph.replace('\n', ' ')
+            if text.strip() != '':
+                ex = {'text': text[::-1]}
+                #ex = {'text': text}
+                out_file.write(json.dumps(ex) + '\n')
