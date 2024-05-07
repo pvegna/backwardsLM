206fe649ee86f243af2abf1d40c99f828122cd9b
Paige Vegna <pvegna@princeton.edu>
2024-05-07 03:07:19 -0400

    eval

---
+++ test/eval.py
@@ -0,0 +1,39 @@
+import evaluate, cuda, json, random
+from transformers import pipeline
+device = 'cuda' if cuda.is_available() else 'cpu'
+model_name = "gpt2-backwards/corpus15"
+direction = "bwd"
+back_gen = pipeline("text-generation", model=f"/scratch/network/pvegna/models/{model_name}", 
+                    tokenizer="/scratch/network/pvegna/models/gpt2-tokenizer").to(device)
+
+with open(f"/scratch/network/pvegna/backwardsLM/data/test_{direction}.json", "r", encoding="utf-8") as in_file:
+    data = in_file.readlines()
+
+predictions = []
+references = []
+
+with open(f"/scratch/network/pvegna/backwardsLM/output/{model_name}.json", "w") as out_file:
+    for ex in data:
+        ex = json.loads(ex)
+        text = ex["text"]
+        if len(text) <= 3:
+            continue
+        split = int(random.uniform(0.33, 0.66) * len(text))
+        prompt = text[0:split]
+        ref = text[split:]
+        pred = back_gen(prompt)[0]['generated_text']
+        predictions.append(pred)
+        references.append(ref)
+        out_file.write(json.dumps({'prompt': prompt, 'ref': ref, 'pred': pred}) + '\n')
+
+
+
+bertscore = evaluate.load("/scratch/network/pvegna/backwardsLM/metrics/bertscore.py")
+results = bertscore.compute(predictions=predictions, references=references, lang="en",
+                            device=device,
+                            model_type="/scratch/network/pvegna/models/roberta-large/",
+                            num_layers=17)
+with open('/scratch/network/pvegna/backwardsLM/output/bertscore.log', 'a') as log_file:
+    output = {'model': model_name, 'precision': results['precision'].mean(),
+              'recall': results['recall'].mean(), 'f1': results['f1'].mean()}
+    log_file.write(json.dumps(output) + '\n')
