4ae3093d39ae80c377acc11ec8246a14dc51a8f0
Paige Vegna <pvegna@princeton.edu>
2024-05-07 09:04:31 -0400

    eval

--- test/eval.py
+++ test/eval.py
@@ -17,14 +17,17 @@ for ex in data:
 print(references)
 
 bertscore = datasets.load_metric("/scratch/network/pvegna/backwardsLM/metrics/bertscore.py")
+ppl = datasets.load_metric("/scratch/network/pvegna/backwardsLM/metrics/perplexity.py")
 results = bertscore.compute(predictions=predictions, references=references, lang="en",
                             device=device,
                             model_type="/scratch/network/pvegna/models/roberta-large/",
                             num_layers=17)
+ppl_results = ppl.compute(predictions=predictions, model_id=f"/scratch/network/pvegna/models/{model_name}", 
+                          device=device)
 p =  sum(results['precision']) / len( results['precision'])
 r = sum(results['recall']) / len(results['recall'])
 f1 = sum(results['f1']) / len(results['f1'])
-with open(f'/scratch/network/pvegna/backwardsLM/output/{model_name}/bertscore.log', 'w') as log_file:
-    output = {'model': model_name, 'precision': p,
-              'recall': r, 'f1': f1}
+with open(f'/scratch/network/pvegna/backwardsLM/output/{model_name}/metrics.log', 'w') as log_file:
+    output = {'model': model_name, 'bert_precision': p,
+              'bert_recall': r, 'bert_f1': f1, "perplexity": ppl_results['mean_perplexity']}
     log_file.write(json.dumps(output) + '\n')
